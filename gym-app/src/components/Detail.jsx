2e0dcab913b6fc6fb668b75df6190bb7097691d8
Ozgur32 <odemirbacak4985@gmail.com>
2023-07-31 05:49:38 +0300

    small changes

--- gym-app/src/components/Detail.jsx
+++ gym-app/src/components/Detail.jsx
@@ -7,13 +7,9 @@ import EquipmentImage from '../assets/icons/equipment.png'
 const Detail = ({ exerciseDetail }) => {
     const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail
 
-    console.log(gifUrl)
-
     return (
         <>
-            <Stack
-                gap="60px"
-                sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
+            <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
                 <img src={gifUrl} alt={name} loading="lazy" className="detail-img" />
             </Stack>
         </>
